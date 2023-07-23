c24c4a420d0c39331024e993f84b106b1bf0dbbe
ysksadboy <133273665+ysksadboy@users.noreply.github.com>
2023-07-23 01:47:15 -0400

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": "b9f0b58b30msh786ce49e4f5902bp1801e4jsn7f5e9689fbc1",
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "40e50e3dc32c421e251d97febb074422"; 
